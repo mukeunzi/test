b7fd51d23914b1a12a80ea6cf58bd0795d80fa0f
mukeunzi <eunji057@gmail.com>
2021-01-07 21:36:31 +0900

    Feat: 스토어 데이터를 파싱하는 메소드 모듈화
    - json형태의 스토어 데이터를 Map으로 파싱하는 메소드를 구현함
    - io작업을 최소화하기 위함
    - storeMap의 key에는 name을 대문자로 변경한 값을, value는 store값을 그대로 저장함

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,25 @@
+const fsPromise = require("fs").promises;
+const path = require("path");
+
+const currentDir = path.join(__dirname, "../");
+const FILENAME = "stores.json";
+
 const asyncWrapper = (fn) => async (req, res, next) => await fn(req, res, next).catch(next);
 
-module.exports = { asyncWrapper };
+const getStoreMap = async () => {
+	try {
+		const strStoreList = await fsPromise.readFile(`${currentDir}/${FILENAME}`, "utf8");
+		const storeList = JSON.parse(strStoreList);
+
+		const storeMap = new Map();
+		storeList.forEach((store) => {
+			const { name, postcode } = store;
+			storeMap.set(name.toUpperCase(), { name, postcode });
+		});
+		return storeMap;
+	} catch (error) {
+		console.log("error: ", error);
+	}
+};
+
+module.exports = { asyncWrapper, getStoreMap };
